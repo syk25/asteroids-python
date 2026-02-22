f8e2fb86f56d8efb08f34b60660e03680ae4236b
syk25 <syk2245@gmail.com>
2026-02-22 22:34:43 +0900

    update movement keys: added arrows

--- player.py
+++ player.py
@@ -27,17 +27,16 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
         self.shoot_cooldown_timer -= dt
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(-dt)
-            
 
-        if keys[pygame.K_d]:
-            self.rotate(dt)            
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.rotate(dt)
 
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.move(dt)
-        
-        if keys[pygame.K_s]:
+
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.move(-dt)
         
         if keys[pygame.K_SPACE]:
