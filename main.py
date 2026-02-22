1f79235a8848886b55deaf7d81682881aa9de890
syk25 <syk2245@gmail.com>
2026-02-22 18:14:01 +0900

    added asteroid and shot kill effect

--- main.py
+++ main.py
@@ -53,6 +53,13 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
