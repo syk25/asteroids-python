772b85cc74e948e9f414524e38e788539d4397d3
syk25 <syk2245@gmail.com>
2026-02-22 12:58:59 +0900

    update main: added clock and time elapse variable dt

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
 
@@ -20,6 +22,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt += clock.tick(60) / 1000
+
+
         
 
 if __name__ == "__main__":
